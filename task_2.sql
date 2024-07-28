a5540e39fcf78cc2477abddc9d579be35140d84e
Muhammed Abdulmalik <abd.muhd01@gmail.com>
2024-07-28 21:11:47 +0300

    Task 2

--- task_2.sql
+++ task_2.sql
@@ -41,6 +41,6 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     book_id INT,
     quantity INT,
     price DECIMAL(10, 2),
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
